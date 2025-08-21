5ebb4c479951f8197af5938067b14143c14df555
sumasriDarla <138578039+sumasriDarla@users.noreply.github.com>
2025-08-21 20:08:02 +0530

    SmallestEvenMultiple.java

---
+++ Time_Complexity_Math/SmallestEvenMultiple.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        int result = 0;
+        if(n%2==0){
+            result = n;
+        }
+        else{
+            result = n*2;
+        }
+        return result;
+        
+    }
+}
